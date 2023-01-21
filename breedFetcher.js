9a587c897730b2a4a961f2aa349467619d4ed437
JunseongKim0105 <118505738+JunseongKim0105@users.noreply.github.com>
2023-01-21 18:41:04 -0500

    Add files via upload

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,16 +2,22 @@ const request = require('request');
 const fs = require('fs');
 const breed = process.argv[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
-  function (error, response, body) {
-    const cats = JSON.parse(body);
-    const cat = cats[0];
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function (error, response, body) {
+      const cats = JSON.parse(body);
+      const cat = cats[0];
+      let description = 'Breed not found';
+      if (cat) {
+        description = cat.description;
+      }
+      callback(error, description);
+    }
+  );
+};
 
-    console.log(cat.description);
-  }
-);
-
+module.exports = { fetchBreedDescription };
 // JSON = Javascript Object Notation
 // console.error('error:', error); // Print the error if one occurred
 // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
